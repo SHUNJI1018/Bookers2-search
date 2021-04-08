3dceb43c4a931e48602cdf6656793fca163894fa
SHUNJI1018 <hide971gg@gmail.com>
2021-04-08 08:51:36 +0000

    [finish] 5

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,17 @@
+class SearchController < ApplicationController
+  before_action :authenticate_user!
+  
+  def search
+    @range = params[:range]
+    @search = params[:search]
+    @word = params[:word]
+    
+    if @range == "User"
+      @users = User.looks(@search, @word)
+    else
+      @books = Book.looks(@search, @word)
+    end
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  get 'search' => 'search#search'
+  
   devise_for :users
   
   resources :users, only: [:show,:index,:edit,:update] do
